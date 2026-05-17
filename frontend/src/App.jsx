33f83bdcfd55d6ab72e127e84796f96d3f658b97
hamsaal <hs8403340@gmail.com>
2026-05-17 18:05:53 +0200

    fix: front end integration with backend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import AssetList from './components/AssetList'
+
+function App() {
+  return (
+    <div>
+      <h1>Portfolio Tracker</h1>
+      <AssetList />
+    </div>
+  )
+}
+
+export default App
